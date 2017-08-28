31a731e94963e4ff180c70df9ab059669d712e9b
Frost <xumengmeng@zju.edu.cn>
2017-08-28 14:24:34 +0300

    input debug

--- Lab1/A.cpp
+++ Lab1/A.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
-
 using namespace std;
 
 double Fahrenheit (double C) {
-    return 9 * C / 5 + 32.0;
+	return 9 * C / 5 + 32.0;
 }
 
 int main() {
-    double C;
-    cout << "Enter a temperature in degrees Celsius : " << endl;
-    cin >> C;
-    cout << "Temperature in degrees fahrenheit is : " <<  Fahrenheit(C) << endl;
+	double C;
+	while(1){
+    		cout << "Enter a temperature in degrees Celsius: " ;
+		cin >> C;
+		while(!cin){
+			cin.clear();
+			cin.ignore();
+			cout << "Input error! Please re-input the number: " ;
+			cin >> C;
+		}
+		cout << "Temperature in degrees fahrenheit is: " <<  Fahrenheit(C) << endl;
+		cout << "-- Press Ctrl+C to quit --" << endl;
+	}
 }
